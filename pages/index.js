c621d663ab960c0fbea4726e6917a8afde0a4944
Nihan Zyan <nihanzyan38@gmail.com>
2022-07-18 18:43:25 +0600

    Test

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    
-       <>
-       <h1>hello world</h1>
-       </>
-  )
+    <>
+      <h1>hello world</h1>
+      <h1>Harifur Rashid</h1>
+    </>
+  );
 }
