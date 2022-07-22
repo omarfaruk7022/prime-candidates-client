5fd29436249f3c8eeeb3d21b7a9914862da84590
Nihan Zyan <nihanzyan38@gmail.com>
2022-07-22 10:10:30 +0600

    Footer Added

---
+++ components/Footer.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-primary text-center ">
+      <footer className="footer grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 p-10 text-base-content">
+        <div>
+          <span className="footer-title text-base-100">Services</span>
+          <a className="link link-hover text-base-100">Branding</a>
+          <a className="link link-hover text-base-100">Design</a>
+          <a className="link link-hover text-base-100">Marketing</a>
+          <a className="link link-hover text-base-100">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title text-base-100">Company</span>
+          <a className="link link-hover text-base-100">About us</a>
+          <a className="link link-hover text-base-100">Contact</a>
+          <a className="link link-hover text-base-100">Jobs</a>
+          <a className="link link-hover text-base-100">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title text-base-100">Legal</span>
+          <a className="link link-hover text-base-100">Terms of use</a>
+          <a className="link link-hover text-base-100">Privacy policy</a>
+          <a className="link link-hover text-base-100">Cookie policy</a>
+        </div>
+        <div>
+          <span className="footer-title text-base-100">Newsletter</span>
+          <div className="form-control w-80">
+            <label className="label">
+              <span className="label-text text-base-100">
+                Enter your email address
+              </span>
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="username@site.com"
+                className="input input-bordered w-full pr-16"
+              />
+              <button className="btn btn-secondary absolute top-0 right-0 rounded-l-none">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div>
+        <p className="text-sm py-5 text-base-100">
+          Copyright © 2022 - All right reserved by Prime Candidates
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
+import Head from "next/head";
+import Image from "next/image";
 import Banner from "../components/Banner";
 import Features from "../components/Features";
+import Footer from "../components/Footer";
 import JobCategory from "../components/JobCategory";
 import Navber from "../components/Navber";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
       <Navber>
         <Banner></Banner>
+        <Features />
+        <JobCategory></JobCategory>
+        <Footer></Footer>
       </Navber>
-      <Features/>
-      <JobCategory></JobCategory>
     </>
   );
 }
