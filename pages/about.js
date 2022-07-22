bc7acfcb4a77b15f18b4b75ffe89e817f7571bac
Ahsan855e <a.r.ahsanhabib08@gmail.com>
2022-07-22 10:35:49 +0600

    setup project

---
+++ pages/about.js
@@ -0,0 +1,4 @@
+const About = () => {
+  return <div> THis is about component</div>;
+};
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    
-       <>
-       <h1>hello world</h1>
-       </>
-  )
+    <>
+      <h1 className="text-4xl text-center">hello world</h1>
+    </>
+  );
 }
