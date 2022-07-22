9ea6ebd70b9eef4c78da0d35ec858a9bed43ac04
Farhan Faruk <farhanfaruk@gmail.comm>
2022-07-22 10:44:47 +0600

    checking

--- components/JobCategory.js
+++ components/JobCategory.js
@@ -5,7 +5,7 @@ import { FaReact } from 'react-icons/fa';
 
 const JobCategory = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 gap-4 my-12 container max-w-[1080px] mx-auto'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 gap-4 my-12 container max-w-[1080px]'>
             <div className='flex items-center justify-around p-12 border-2 rounded-lg text-2xl hover:bg-primary hover:text-white hover:border-secondary hover:scale-105 transition-all  duration-300 h-5 w-[480px]'>
                 <FaReact></FaReact>
                 <Link href={"/"} className='cursor-pointer'>9,071+ React Engineers</Link>
