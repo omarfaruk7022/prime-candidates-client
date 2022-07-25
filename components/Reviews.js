e0b0240b630b147879f7af36d5b0e902350adaa2
dev-weirdo <dev_weirdo@protonmail.com>
2022-07-25 12:27:30 +0600

    code merged

--- components/Reviews.js
+++ components/Reviews.js
@@ -6,7 +6,7 @@ export default function Reviews({ comments }) {
 
     return (
         <div className='container mx-auto px-5 py-10'>
-            <h1 className='text-4xl font-light text-center mb-7'>What Our Customer Says</h1>
+            <h1 className='text-4xl font-light text-center mb-10'>What Our Customer Says</h1>
             <Swiper
                 breakpoints={{
                     640: {
@@ -19,14 +19,14 @@ export default function Reviews({ comments }) {
                     },
                     1024: {
                         slidesPerView: 3,
-                        spaceBetween: 50
+                        spaceBetween: 100
                     },
                 }}
             >
                 {
                     comments.slice(0, 21).map(comment =>
                         <SwiperSlide key={comment.id}>
-                            <div className='bg-gray-100 text-black rounded-lg gap-10  flex flex-col p-5 items-center text-justify h-96'>
+                            <div className='bg-gray-100 text-black rounded-lg gap-5 flex flex-col p-5 items-center text-justify h-96 lg:text-lg'>
                                 <div className="avatar">
                                     <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                                         <Image src="https://placeimg.com/192/192/people" alt="Picture of the commenter" height={100} width={100} />
