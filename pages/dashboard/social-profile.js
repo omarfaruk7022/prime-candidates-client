9b96abe0e6ec399a265f845c5d7650a46c14ed5c
Hossain-Sajjad <sunshinems58.com>
2022-08-12 03:12:57 +0600

    user profile edited

--- pages/dashboard/social-profile.js
+++ pages/dashboard/social-profile.js
@@ -1,9 +1,9 @@
 import DashboardLayout from '../../components/DashboardLayout'
 
-const Index = () => (
-    <DashboardLayout>
+const Index = () => {
+    return <DashboardLayout>
         <h1>this is social profile section</h1>
     </DashboardLayout>
-)
+}
 
 export default Index
