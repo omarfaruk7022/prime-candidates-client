06df94c715695f525ff21319c54773b818ed5296
Farhan Faruk <farhanfaruk@gmail.comm>
2022-08-31 05:50:00 +0600

    last

--- pages/_app.js
+++ pages/_app.js
@@ -6,18 +6,18 @@ import Router from "next/router";
 import Loader from "../components/Loader";
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
-  Router.events.on("routeChangeStart", (url) => {
-    console.log(`Loading: ${url}`);
-    setLoading(true);
-  });
-  Router.events.on("routeChangeComplete", (url) => {
-    console.log(`Loaded: ${url}`);
-    setLoading(false);
-  });
+  // const [loading, setLoading] = useState(false);
+  // Router.events.on("routeChangeStart", (url) => {
+  //   console.log(`Loading: ${url}`);
+  //   setLoading(true);
+  // });
+  // Router.events.on("routeChangeComplete", (url) => {
+  //   console.log(`Loaded: ${url}`);
+  //   setLoading(false);
+  // });
   return (
     <>
-      {loading && <Loader />}
+      {/* {loading && <Loader />} */}
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
